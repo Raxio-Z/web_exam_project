0c4e3b8081607c7add0e18c320f45f38dab82713
joker <2923784829@qq.com>
2022-06-04 21:05:44 +0800

    新增了初始化sql文件

---
+++ backend/src/main/resources/static/create.sql
@@ -0,0 +1,18 @@
+create database if not exists online_exam;
+use online_exam;
+drop table user;
+create table user(
+	user_id integer key auto_increment,
+    user_username text,
+    user_nickname text,
+    user_password text,
+    user_role_id integer,
+    user_avatar text,
+    user_description text,
+    user_email text,
+    user_phone text
+);
+
+insert into user(user_username,user_password) values('cym','123');
+select * from user;
+ 
